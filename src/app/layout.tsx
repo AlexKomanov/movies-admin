164eb20ff11be06b79ca77525b951dbb30813e63
AlexKomanov <akomanov88@gmail.com>
2023-11-28 16:13:31 +0200

    Adding signup page with api and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ToastContainer, toast } from 'react-toastify';
 import Navbar from '@/components/Navbar/Navbar'
-
+import 'react-toastify/dist/ReactToastify.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <ToastContainer />
         </body>
     </html>
   )
--- src/app/pages/auth/signup/page.tsx
+++ src/app/pages/auth/signup/page.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
-const page = () => {
+const SignupPage = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/admin/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password }),
+        credentials: 'include'
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Handle successful login, e.g., store adminAuthToken in a secure way
+        console.log('Admin login successful', data);
+
+        toast.success('Admin Registered Successfully!!!', {
+          position: toast.POSITION.TOP_CENTER,
+        });
+
+        setTimeout(() => {
+          window.location.href = '/pages/movie/createmovie';
+        }, 3000);
+
+
+      } else {
+        // Handle login error
+        console.error('Admin registration was failed', response.statusText);
+        toast.error('Admin registraton was failed', {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      }
+    }
+    catch (error) {
+      toast.error('An error occurred during registration'), {
+        position: toast.POSITION.TOP_CENTER
+      };
+      console.error('An error occurred during registration', error);
+    }
+  }
   return (
-    <div>page</div>
+    <div className='formpage'>
+      <input
+        type='text'
+        placeholder='Name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignup}>Sign up</button>
+    </div>
   )
 }
 
-export default page
+export default SignupPage
